030891f1158085d2f20d1089ebfbdb18743f9021
Jonny Elliott <jonnynelliott@googlemail.com>
2014-05-15 19:50:09 +0200

    Started new feature to include the box creation in terms of:
    player clicking on the screen
    
    random creation of falling boxes (tbd)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,12 @@ using System.Collections;
 /// This script will control the player's movements.
 /// It is probably more efficient to use inbuilt controls, but this is for practise
 /// 
+/// Ability to jump on spacebar
+/// 
+/// Ability to move with arrows or wasd
+/// 
+/// Ability to create boxes at the screen position when the user clicks mouse 1
+/// 
 /// Author: Jonathan Elliott 
 /// Year:   2014
 /// </summary>
@@ -16,6 +22,13 @@ public class PlayerMove : MonoBehaviour {
 	private float inVelocity;
 	private float forceJump;
 	private float forceJumpInt;
+	private float nextBoxTime;
+	private float boxCreateRate;
+	private float nextJumpTime;
+	private float jumpRate;
+	private Vector3 mouseScreenPosition;
+	private Vector3 mouseWorldPosition;
+	public GameObject playerBox;
 	// Variables end_____________________
 
 	// Use this for initialization
@@ -24,6 +37,10 @@ public class PlayerMove : MonoBehaviour {
 		velocityPlayer = .2f;
 		forceJumpInt = 4;
 		forceJump = 0.00001f * forceJumpInt;
+		boxCreateRate = 1;
+		jumpRate = 1;
+		nextBoxTime = 0;
+		nextJumpTime = 0;
 	}
 	
 	// Update is called once per frame
@@ -47,11 +64,30 @@ public class PlayerMove : MonoBehaviour {
 		transform.position = playerPosition;
 
 		// Check if the jump key is pressed. Currently set to space bar.
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextJumpTime)
 		{
+			nextJumpTime = Time.time + jumpRate;
+
 			GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
 			Rigidbody playerRigidBody = playerObject.GetComponent<Rigidbody>();
 			playerRigidBody.AddForce(0, forceJump, 0);
+		}
+	}
+
+	void Update ()
+	{
+		// If the mouse button is pressed then act upon box creation
+		if (Input.GetButton("Fire1") && Time.time > nextBoxTime)
+		{
+			nextBoxTime = Time.time + boxCreateRate;
+
+			mouseScreenPosition = Input.mousePosition;
+			mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+			mouseWorldPosition.z = 0;
+			Debug.Log(mouseWorldPosition);
+
+			//playerBox = GameObject.FindGameObjectWithTag("FishyBox");
+			Instantiate(playerBox, mouseWorldPosition, Quaternion.identity);
 		}
 	}
 }
