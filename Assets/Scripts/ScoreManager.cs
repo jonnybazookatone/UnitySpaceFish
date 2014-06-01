4e34a2b967dfc643775a031db9ad757ee7f8a0aa
Jonny Elliott <jonnynelliott@googlemail.com>
2014-06-01 16:53:46 +0200

    Implementing the first usage of PlayerPrefs. Currently, it will only allow high score per person. At some point, could include a high score system on the server side. But need to get a server first.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,6 +10,8 @@ public class ScoreManager : MonoBehaviour {
 
 	// Variables start_____________________
 
+	public string userName;
+	private int newScore;
 	private bool _endOfGame;
 	public bool endOfGame
 	{
@@ -25,6 +27,8 @@ public class ScoreManager : MonoBehaviour {
 		// Ensure not end of game
 		// Testing: set to true
 		this.endOfGame = true;
+	
+		userName = "MyName";
 	}
 	
 	// Update is called once per frame
@@ -32,8 +36,42 @@ public class ScoreManager : MonoBehaviour {
 	{
 		if (this.endOfGame)
 		{
+			newScore = (int)Time.time;
+
+			if (PlayerPrefs.HasKey("PlayerName"))
+			{
+				userName = PlayerPrefs.GetString("PlayerName");
+			}
+
 			// Load the GUI menu if the player has died
 			GUILayout.Box ("box");
+
+			// If there are old high scores, then we can just add a new one
+			if (PlayerPrefs.HasKey("HighScore"))
+			{
+
+				// If score is higher than the last saved, add it
+				if (PlayerPrefs.GetInt("HighScore") > newScore)
+				{
+					PlayerPrefs.SetInt("HighScore", newScore);
+					PlayerPrefs.SetString("PlayerName", userName);
+				}
+			}
+			else
+			{
+				GUILayout.Label ("Name");
+				userName = GUILayout.TextArea(userName);
+				if (GUILayout.Button ("Save"))
+				{
+					PlayerPrefs.SetString("PlayerName", userName);
+					PlayerPrefs.SetInt("HighScore", newScore);
+				}
+			}
+
+			this.endOfGame = false;
+			// Reload level
+			Application.LoadLevel(Application.loadedLevel);
+
 		}
 	
 	}
